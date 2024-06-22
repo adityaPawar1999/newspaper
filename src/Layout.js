cf9de6641e6f07f553ab16c85256c27927437940
aditya pawar <pawaraditya2285@gmail.com>
2024-06-22 09:45:57 +0530

    minor updates

--- src/Layout.js
+++ src/Layout.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Layout = () => {
   return (
-    <div className="container mx-auto px-2">
+    <div className="container mx-auto px-4">
       <div className='lg:block md:hidden sm:hidden '>
       <div className="grid grid-cols-1 md:grid-cols-12 gap-4">
         <div className="col-span-12 md:col-span-2 lg:col-span-2 bg-gray-200 p-4">left</div>
