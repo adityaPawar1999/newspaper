054141d509a4c1b4fd9b7b861948e1467edc4477
aditya pawar <pawaraditya2285@gmail.com>
2024-06-25 16:30:39 +0530

    resize image

--- src/components/FeaturedStories.js
+++ src/components/FeaturedStories.js
@@ -12,7 +12,7 @@ const FeaturedStories = () => {
             NewsData.slice(0,3).map((val,index)=>{
               return(<>
           <div key={index} className=''>
-            <img src={val.image} alt="Story 1" className="mb-2 h-[200px] w-[100%] cover bg-gray-300 filter grayscale" />
+            <img src={val.image} alt="Story 1" className="mb-2 h-[150px]  w-[100%] cover bg-gray-300 filter grayscale" />
             <h3 className="text-sm font-bold ">{val.title}</h3>
             <p className='text-[12px] overflow-hidden line-clamp-5 '>{val.description}</p>
           </div>
